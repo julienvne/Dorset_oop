57f2246589967411c3852d6e05e35625c20504f2
DESKTOP-FEK7531\Quentin K <quentin.kuttler@orange.fr>
2022-03-28 14:45:49 +0200

    add blank board + main function

--- src/main/java/com/dorset/gossipers/Player.java
+++ src/main/java/com/dorset/gossipers/Player.java
@@ -54,56 +54,23 @@ public class Player {
     //given in parameters, return a string which contains the result
     public String firePlayer(Player ennemyPlayer, int x, int y){
         String box = ennemyPlayer.board.getCoordinate(x,y);
+        Board blankBoard = ennemyPlayer.getBlankBoard();
         switch (box){
             case "T":
                 return("Already touched");
             case "w":
+                blankBoard.getBoard()[x][y] = ".";
+                ennemyPlayer.board.getBoard()[x][y] ="T";
                 return("Plouf");
             default:
+                blankBoard.getBoard()[x][y] = "X";
                 Boat boat = ennemyPlayer.getBoat(box);
                 boat.removeLife();
                 ennemyPlayer.board.getBoard()[x][y] ="T";
-                if(boat.getLife() != 0 )
+                if(boat.getLife() != 0)
                     return("Touched boat");
-                return("Couler!");
+                return("Sink!");
         }
     }
 
-    public static void main(String[] args)
-            throws IOException{
-        Board board1 = new Board();
-        Board board2 = new Board();
-        Board Blankboard1 = new Board();
-        Board Blankboard2 = new Board();
-
-        Boat[] array1 = Board.createArrayOfBoats();
-        Boat[] array2 = Board.createArrayOfBoats();
-
-        board1.fillBoardWithBoat(array1);
-        board2.fillBoardWithBoat(array2);
-
-        Player player1 = new Player(board1,Blankboard1,array1);
-        Player player2 = new Player(board2,Blankboard2,array2);
-
-        while(!player1.gameOver() || !player2.gameOver()){
-            // Enter data using BufferReader
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(System.in));
-
-            player2.board.printBoard();
-
-            // Reading data using readLine
-            int x = Integer.parseInt(reader.readLine());
-            while(x > 9)
-                x = Integer.parseInt(reader.readLine());
-
-            int y = Integer.parseInt(reader.readLine());
-            while(y > 9)
-                y = Integer.parseInt(reader.readLine());
-
-            System.out.println(player1.firePlayer(player2,x,y));
-            player2.getLifeEachBoats();
-
-        }
-    }
 }
---
+++ src/main/java/com/dorset/gossipers/main.java
@@ -0,0 +1,45 @@
+package com.dorset.gossipers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+    public static void main(String[] args)
+            throws IOException {
+        Board board1 = new Board();
+        Board board2 = new Board();
+        Board Blankboard1 = new Board();
+        Board Blankboard2 = new Board();
+
+        Boat[] array1 = Board.createArrayOfBoats();
+        Boat[] array2 = Board.createArrayOfBoats();
+
+        board1.fillBoardWithBoat(array1);
+        board2.fillBoardWithBoat(array2);
+
+        Player player1 = new Player(board1,Blankboard1,array1);
+        Player player2 = new Player(board2,Blankboard2,array2);
+
+        while(!player1.gameOver() || !player2.gameOver()){
+            // Enter data using BufferReader
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
+
+            player2.getBoard().printBoard();
+
+            // Reading data using readLine
+            int x = Integer.parseInt(reader.readLine());
+            while(x > 9)
+                x = Integer.parseInt(reader.readLine());
+
+            int y = Integer.parseInt(reader.readLine());
+            while(y > 9)
+                y = Integer.parseInt(reader.readLine());
+
+            System.out.println(player1.firePlayer(player2,x,y));
+            player2.getLifeEachBoats();
+
+        }
+    }
+}
