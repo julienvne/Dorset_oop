c1375769f3ba6b596832422a75fe6d5e18e728ac
DESKTOP-FEK7531\Quentin K <quentin.kuttler@orange.fr>
2022-04-04 15:37:43 +0200

    Add function to get Coordinate Add function for player to play a round When a player touch a boat, he can now play again Print tweaks handling new errors.

--- src/main/java/com/dorset/gossipers/Main.java
+++ src/main/java/com/dorset/gossipers/Main.java
@@ -12,6 +12,59 @@ public class Main {
 
     public static ClientType clientType;
 
+    public static int getCoordinate(String number, String coord){
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        Boolean check = true;
+        int x = 0;
+        while(check){
+            System.out.print("Player "+number+", choose the "+coord+" coordinate: ");
+            try{
+                x = Integer.parseInt(reader.readLine());
+                if(x > 9 || x < 0){
+                    System.out.println("Please enter a valid number...");
+                }else{
+                    check = false;
+                }
+            }catch(Exception e){ //catches an exception when the user enters a string or anything but an int
+                System.out.println("Please only use digits to make a selection.");
+            }
+        }
+        return x;
+
+    };
+
+    public static void playRound(Player attacker, Player defender, String number) throws IOException {
+
+        boolean continuePlaying = true;
+        // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        System.out.println("\n----------");
+        System.out.println("\nPlayer "+number+", it's your turn !\n");
+
+        while(continuePlaying){
+            System.out.println("Blank board of the opponent: ");
+            defender.getBlankBoard().printBoard();
+            System.out.println();
+
+            // Reading data using readLine
+
+            int x = getCoordinate(number, "y");
+
+            int y = getCoordinate(number, "x");
+
+
+            String status = attacker.firePlayer(defender, x, y);
+            System.out.println(status);
+
+            continuePlaying = (status == "Touched boat" || status == "Sink!") && !defender.gameOver();
+
+            defender.getLifeEachBoats();
+        }
+    };
+
     public static void main(String[] args) throws IOException {
         if (args.length == 1 && args[0].equalsIgnoreCase("server")) {
             clientType = ClientType.SERVER;
@@ -39,25 +92,22 @@ public class Main {
         Player player1 = new Player(board1, Blankboard1, array1);
         Player player2 = new Player(board2, Blankboard2, array2);
 
+
         while (!player1.gameOver() || !player2.gameOver()) {
-            // Enter data using BufferReader
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(System.in));
 
-            player2.getBoard().printBoard();
+            playRound(player1,player2,"1");
 
-            // Reading data using readLine
-            int x = Integer.parseInt(reader.readLine());
-            while (x > 9)
-                x = Integer.parseInt(reader.readLine());
-
-            int y = Integer.parseInt(reader.readLine());
-            while (y > 9)
-                y = Integer.parseInt(reader.readLine());
-
-            System.out.println(player1.firePlayer(player2, x, y));
-            player2.getLifeEachBoats();
-
+            if(!player2.gameOver()){
+                playRound(player2,player1,"2");
+            }
         }
+
+        if(player1.gameOver()){
+            System.out.println("\n----------\nPlayer 2 won!");
+        }
+        else{
+            System.out.println("\n----------\nPlayer 1 won!");
+        }
+
     }
 }
